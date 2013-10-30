465d627aedfad4a635c4957708958c0491b59aa1
IgoNsk <igor@yatsevich.ru>
2013-10-30 22:27:04 +0700

    Fix paddings and style of code

--- CatalogModule.php
+++ CatalogModule.php
@@ -1,23 +1,19 @@
-<?
-Yii::setPathOfAlias('CatalogModule' , dirname(__FILE__));
+<?php
+  
+  Yii::setPathOfAlias('CatalogModule' , dirname(__FILE__));
 
-class CatalogModule extends CWebModule
-{
-	public $tablePrefix = 'catalog__';
-
-// 	public $controllerMap=array(
-// 			'rubrics'=>array(
-// 				'class'=>'CatalogModule.controllers.RubricController'),
-// 			);
-
-	public function init()
+  class CatalogModule extends CWebModule
   {
-		$this->setImport(
-      array(
-  			'application.modules.catalog.components.*',
-  			'application.modules.catalog.controllers.*',
-  			'application.modules.catalog.models.*',
-		  )
-    );
-	}
-}
+    public $tablePrefix = 'catalog__';
+  
+    public function init()
+    {
+      $this->setImport(
+        array(
+          'application.modules.catalog.components.*',
+          'application.modules.catalog.controllers.*',
+          'application.modules.catalog.models.*',
+        )
+      );
+    }
+  }
